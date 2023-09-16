59e5ebb0e8d060901f6fd1e0612e62a68748ac51
diogonapoles <napolesdr@gmail.com>
2023-09-16 22:29:25 +0100

    feat: simplified cmp mappings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -18,19 +18,19 @@ lsp.nvim_workspace()
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-e>'] = cmp.mapping.close(),
+  ['<CR>'] = cmp.mapping.confirm({
+    behavior = cmp.ConfirmBehavior.Replace,
+    select = true
+  }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
-
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
 })
-
 lsp.set_preferences({
     suggnst_lsp_servers = false,
     sign_icons = {
