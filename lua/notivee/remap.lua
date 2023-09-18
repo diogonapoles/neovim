4ed5b997e8bc57ce806148f1222f79c30b2f5765
diogonapoles <napolesdr@gmail.com>
2023-09-18 15:13:19 +0100

    feat: added a few more maps

--- lua/notivee/remap.lua
+++ lua/notivee/remap.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+vim.keymap.set('n', '<leader>nh', ':nohl<CR>')
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -6,11 +7,5 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
-vim.keymap.set({"n", "v"}, "<leader>p", [["+p]])
-vim.keymap.set("n", "<leader>P", [["+P]])
+vim.keymap.set('n', '<C-a>', 'gg<S-v>G')
