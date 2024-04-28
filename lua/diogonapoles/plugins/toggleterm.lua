14778b9fb3e638880d98f2095c629fd026774d7e
diogonapoles <napolesdr@gmail.com>
2024-04-28 12:36:03 +0100

    feat(toggleterm): plugin added to lazy

---
+++ lua/diogonapoles/plugins/toggleterm.lua
@@ -0,0 +1,13 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	opts = {
+		open_mapping = [[<c-\>]],
+		direction = "float",
+		auto_scroll = true,
+		float_opts = {
+			border = "curved",
+		},
+	},
+	config = true,
+}
