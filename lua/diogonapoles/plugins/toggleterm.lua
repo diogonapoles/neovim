a4416986c6fc5bc3336dc6c81c30bc99e49eb15a
diogonapoles <napolesdr@gmail.com>
2024-05-25 11:05:27 +0100

    chore(keymaps): better keymaps

--- lua/diogonapoles/plugins/toggleterm.lua
+++ lua/diogonapoles/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ return {
 	"akinsho/toggleterm.nvim",
 	ersion = "*",
 	opts = {
-		open_mapping = [[<c-s-T>]],
+		open_mapping = [[<space>t]],
 		direction = "float",
 		auto_scroll = true,
 		float_opts = {
